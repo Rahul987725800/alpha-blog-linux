7bc5e3639bc1fc41ef6c7734aadbbcca0de97dbf
Rahul Gupta <guptarahul70322@gmail.com>
2020-09-01 20:12:49 +0530

    create article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,4 +5,13 @@ class ArticlesController < ApplicationController
     def index 
         @articles = Article.all
     end
+    def new
+    end
+    def create 
+        # we need to specify the fields we extract this is for security
+        @article = Article.new(params.require(:article).permit(:title, :description))
+        @article.save
+        redirect_to @article 
+        # redirect_to article_path(@article) also works
+    end
 end
