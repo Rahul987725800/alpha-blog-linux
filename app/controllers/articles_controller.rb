f6903d49f4f754108c76bc1bf68e9d10e6d5215a
Rahul Gupta <guptarahul70322@gmail.com>
2020-09-02 09:28:26 +0530

    errors and flash messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,12 +6,15 @@ class ArticlesController < ApplicationController
         @articles = Article.all
     end
     def new
+        @article = Article.new
     end
     def create 
-        # we need to specify the fields we extract this is for security
         @article = Article.new(params.require(:article).permit(:title, :description))
-        @article.save
-        redirect_to @article 
-        # redirect_to article_path(@article) also works
+        if @article.save
+            flash[:notice] = "Article was created successfully."
+            redirect_to @article 
+        else
+            render 'new'
+        end
     end
 end
